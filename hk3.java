3b5aa7df67b1d4cc9efd2f8fa2b668b13d9bc43a
Wang-kaiwei2006 <wangkaiwei363@gmail.com>
2026-09-22 16:51:54 +0800

    first commit

---
+++ hk3.java
@@ -0,0 +1,11 @@
+package test;
+
+public class hk3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("歡迎使用Java!");
+		System.out.print("開始使用Java吧!");
+	}
+
+}
